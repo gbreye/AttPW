0d0f973a05a953f36e58cda6b7375e4f0cb0f15e
gbreye <gbrdsmain@gmail.com>
2026-09-21 09:58:57 -0300

    added images and more customization

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 const mainDiv = document.getElementById('main');
 const form = document.getElementById('form');
 
-
 const products = [
-    ['Smartphone S26', 'Smartphone GALAXY S26 ULTRA', 'smartphone', '1000,00'],
-    ['Smartphone S25', 'Smartphone GALAXY S25 ULTRA', 'smartphone', '900,00'],
-    ['Thinkpad X1', 'Thinkpad X1 Carbon Gen 9', 'notebook', '2000,00'],
-    ['Laptop MacBook Pro', 'Laptop MacBook Pro M3', 'notebook', '3000,00'],
-    ['Smartwatch S26', 'Smartwatch GALAXY S26 ULTRA', 'smartwatch', '500,00'],
-    ['Tablet S10', 'Tablet GALAXY S10 ULTRA', 'tablet', '800,00'],
-    ['Tablet S9', 'Tablet GALAXY S9 ULTRA', 'tablet', '700,00'],
-    ['TV', 'TV 55"', 'tv', '1500,00'],
+    ['assets/S26.png', 'Smartphone S26', 'Smartphone GALAXY S26 ULTRA', 'smartphone', '1000,00'],
+    ['assets/S25.webp', 'Smartphone S25', 'Smartphone GALAXY S25 ULTRA', 'smartphone', '900,00'],
+    ['assets/Thinkpad.png', 'Thinkpad X1', 'Thinkpad X1 Carbon Gen 9', 'notebook', '2000,00'],
+    ['assets/Mac.webp', 'Laptop MacBook Pro', 'Laptop MacBook Pro M3', 'notebook', '3000,00'],
+    ['assets/Watch.png', 'Smartwatch S26', 'Smartwatch GALAXY S26 ULTRA', 'smartwatch', '500,00'],
+    ['assets/Tab.avif', 'Tablet S10', 'Tablet GALAXY S10 ULTRA', 'tablet', '800,00'],
 ]
 
 function renderCard(filter) {
     filter.forEach(product => {
         const card = document.createElement('div');
+        const textArea = document.createElement('div');
+        const imageArea = document.createElement('div');
         product.forEach((value, index) => {
             switch(index) {
                 case 0:
-                    const title = document.createElement('h2');
-                    title.textContent = value;
-                    card.appendChild(title);
+                    const image = document.createElement('img');
+                    image.src = value;
+                    imageArea.appendChild(image);
                     break;
                 case 1:
-                    const description = document.createElement('p');
-                    description.textContent = value;
-                    card.appendChild(description);
+                    const title = document.createElement('h2');
+                    title.textContent = value;
+                    textArea.appendChild(title);
                     break;
                 case 2:
-                    const category = document.createElement('p');
-                    category.textContent = value;
-                    card.appendChild(category);
+                    const description = document.createElement('h4');
+                    description.textContent = value;
+                    textArea.appendChild(description);
                     break;
                 case 3:
+                    const category = document.createElement('p');
+                    category.textContent = value;
+                    textArea.appendChild(category);
+                    break;
+                case 4:
                     const price = document.createElement('p');
                     price.textContent = value;
-                    card.appendChild(price);
+                    textArea.appendChild(price);
                     break;
             }
         });
+        card.appendChild(imageArea);
+        card.appendChild(textArea);
+        card.classList.add('card');
         mainDiv.appendChild(card);
         return card;
     });
